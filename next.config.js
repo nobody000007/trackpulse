8b16c61bcbf808230b60bac849e79dc582ea2f3b
ralph <elkhouryralph123@gmail.com>
2026-03-24 19:59:07 -0400

    fix deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  outputFileTracingIncludes: {
+    "/**/*": ["./node_modules/.prisma/client/**/*"],
+  },
   images: {
     remotePatterns: [
       {
