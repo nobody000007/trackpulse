abb1e05d56a270543a4344b05ba9bbfc3b4c8b17
ralph <elkhouryralph123@gmail.com>
2026-04-22 23:23:06 -0400

    Use prisma db push instead of migrate deploy to create tables

--- terraform/modules/kubernetes/main.tf
+++ terraform/modules/kubernetes/main.tf
@@ -74,7 +74,7 @@ resource "kubernetes_deployment" "app" {
           name              = "db-migrate"
           image             = "${var.acr_login_server}/trackpulse:latest"
           image_pull_policy = "Always"
-          command           = ["node", "/app/node_modules/prisma/build/index.js", "migrate", "deploy"]
+          command           = ["node", "/app/node_modules/prisma/build/index.js", "db", "push", "--accept-data-loss"]
 
           env_from {
             config_map_ref { name = kubernetes_config_map.app.metadata[0].name }
