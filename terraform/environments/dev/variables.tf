e4a5489c644b675f0eee78f8cf311cc4bb30e5ff
ralph <elkhouryralph123@gmail.com>
2026-04-25 19:55:50 -0400

    Optimize pipeline: remove terraform download, single init with provider cache, remove build and deploy stages

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -3,5 +3,5 @@ variable "nextauth_secret"    { sensitive = true }
 variable "groq_api_key"       { sensitive = true }
 variable "gmail_user"         { sensitive = true }
 variable "gmail_app_password" { sensitive = true }
-variable "my_ip"              { default = "70.53.155.166" }
+variable "my_ip"              { default = "70.52.17.126" }
 variable "aks_node_ip"        { default = "10.240.0.4" }
