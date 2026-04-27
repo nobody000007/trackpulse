7362870b9ca5a8775395b5bac2352f39bc6bdb28
ralph <elkhouryralph123@gmail.com>
2026-04-27 19:46:02 -0400

    Fix HCL syntax: multi-line block for app_insights_connection_string variable

--- terraform/environments/staging/variables.tf
+++ terraform/environments/staging/variables.tf
@@ -3,5 +3,8 @@ variable "nextauth_secret"               { sensitive = true }
 variable "groq_api_key"                  { sensitive = true }
 variable "gmail_user"                    { sensitive = true }
 variable "gmail_app_password"            { sensitive = true }
-variable "app_insights_connection_string" { sensitive = true; default = "" }
+variable "app_insights_connection_string" {
+  sensitive = true
+  default   = ""
+}
 variable "app_url"                        { default = "http://localhost" }
