72b8472b1a90c3402687665f7e96cefe8d485e4a
ralph <elkhouryralph123@gmail.com>
2026-04-27 19:44:37 -0400

    Make app_insights_connection_string optional with empty default

--- terraform/modules/kubernetes/variables.tf
+++ terraform/modules/kubernetes/variables.tf
@@ -6,7 +6,7 @@ variable "groq_api_key"                  { sensitive = true }
 variable "gmail_user"                    { sensitive = true }
 variable "gmail_app_password"            { sensitive = true }
 variable "storage_conn_string"           { sensitive = true }
-variable "app_insights_connection_string" { sensitive = true }
+variable "app_insights_connection_string" { sensitive = true; default = "" }
 variable "acr_login_server"              {}
 variable "acr_username"                  {}
 variable "acr_password"                  { sensitive = true }
