663b3dbdd578addf4171202d2095b5513d9109a1
ralph <elkhouryralph123@gmail.com>
2026-04-29 10:02:27 -0400

    Fix setAutoCollectPerformance call to pass required second argument

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -4,7 +4,7 @@ export async function register() {
     appInsights.setup(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING)
       .setAutoDependencyCorrelation(true)
       .setAutoCollectRequests(true)
-      .setAutoCollectPerformance(true)
+      .setAutoCollectPerformance(true, true)
       .setAutoCollectExceptions(true)
       .setAutoCollectDependencies(true)
       .setAutoCollectConsole(true, true)
